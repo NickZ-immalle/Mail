18576390989138d4135014e4f5dd556999fc9c1e
NickZ-immalle <31651073+NickZ-immalle@users.noreply.github.com>
2018-11-07 11:10:55 +0100

    Update MailClient.py

--- MailClient.py
+++ MailClient.py
@@ -1,3 +1,6 @@
+from MailServer import MailServer
+from MailItem import MailItem
+
 class MailClient:
     def __init__(self, server, user):
         self.server = server
@@ -13,23 +16,3 @@ class MailClient:
     def sendMailItem(self, to, msg):
         item = MailItem(self.user, to, msg)
         self.server.post(item)
-
-
-    
-if __name__ == '__main__':
-    s = MailServer()
-    
-    print(" Logging in Joske to mail server...")
-    c1 = MailClient(s, "Joske")
-
-    print(" User joske sending mail to Jantje...")
-    c1.sendMailItem("Jantje", "Hello!!!")
-
-    print(" Logging in Jantje to mail server...")
-    c2 = MailClient(s, "Jantje")
-
-    print(" User Jantje reading mail...")
-    c2.printNextMailItem()
-
-    print(" User Jantje reading mail...")
-    c2.printNextMailItem()
